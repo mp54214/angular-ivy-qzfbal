41e5b4860c0627c3a6cbbb54a5a66682b984206d
mp54214 <32453832+mp54214@users.noreply.github.com>
2022-09-14 20:57:48 +0300

    added syntax highlight.js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppRoutingRoutingModule } from './app-routing.module';
 import { NavigationComponent } from './navigation/navigation.component';
 import { UserSettingsModule } from './user-settings/user-settings.module';
 import { UserDataModule } from './user-settings/user-data/user-data.module';
+import { HighlightModule, HIGHLIGHT_OPTIONS } from 'ngx-highlightjs';
 
 @NgModule({
   imports: [
@@ -15,8 +16,17 @@ import { UserDataModule } from './user-settings/user-data/user-data.module';
     AppRoutingRoutingModule,
     UserSettingsModule,
     UserDataModule,
+     HighlightModule
   ],
   declarations: [AppComponent, NavigationComponent],
   bootstrap: [AppComponent],
+  providers: [
+    {
+      provide: HIGHLIGHT_OPTIONS,
+      useValue: {
+        fullLibraryLoader: () => import('highlight.js'),
+      }
+    }
+  ],
 })
 export class AppModule {}
