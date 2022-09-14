91ba41d4cb707037646f15a0b4639a680999db24
mp54214 <32453832+mp54214@users.noreply.github.com>
2022-09-14 22:05:03 +0300

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,17 +7,17 @@ import { catchError, map } from 'rxjs/operators';
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ],
-  // styles: ['@import "https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"']
+   styles: ['@import "https://unpkg.com/@highlightjs/cdn-assets@11.6.0/styles/default.min.css"']
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
   apiLoaded: any
 
-constructor(httpClient: HttpClient) {
-    this.apiLoaded = httpClient.jsonp('https://unpkg.com/@highlightjs/cdn-assets@11.6.0/highlight.min.js', 'callback')
-        .pipe(
-          map(() => true),
-          catchError(() => of(false)),
-        );
-  }
+// constructor(httpClient: HttpClient) {
+//     this.apiLoaded = httpClient.jsonp('https://unpkg.com/@highlightjs/cdn-assets@11.6.0/highlight.min.js', 'callback')
+//         .pipe(
+//           map(() => true),
+//           catchError(() => of(false)),
+//         );
+//   }
 }
